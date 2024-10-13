e15579814679a4a0cf7a08462d387d98da47f227
Jiahao <yjiahao123@gmail.com>
2024-10-13 03:35:19 +0000

    add templates, styles folders

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,18 @@
 from app import app
-from flask import request, abort
+from flask import request, abort, render_template, url_for
 
 # test route for home
 @app.route("/", methods = ["GET", "POST"])
 def home():
+    # if user is accessing the page directly
     if request.method == "GET":
-        return "Hello world!"
+        # return render_template("index.html"): supposed to render the html template here
+        return render_template("index.html")
+    # elif user submits the form on the page
     elif request.method == "POST":
+        # create vector embeddings of the sentences user types in, then perform vector search here as well
+        # arrange the rows and return top k relevant results
         return "Under development"
+    else:
+        # otherwise, if request is not GET/POST
+        abort(400, description="Request method not supported")
