ac59f098de35d02e5ffa925dbdad427223d33855
Jiahao <yjiahao123@gmail.com>
2024-10-13 04:13:24 +0000

    added libraries

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,22 @@
 from app import app
 from flask import request, abort, render_template, url_for
 
+import pandas as pd
+
+from sentence_transformers import SentenceTransformer
+
+# load pretrained encoding model, takes a while to load
+# model = SentenceTransformer("sentence-transformers/all-mpnet-base-v2")
+
 # test route for home
 @app.route("/", methods = ["GET", "POST"])
 def home():
     # if user is accessing the page directly
     if request.method == "GET":
-        # return render_template("index.html"): supposed to render the html template here
+        # return render_template("index.html"): supposed to render the html template here with the forms and everything
+        # sentences = ["This is an example sentence"]
+        # embeddings = model.encode(sentences)
+        # return render_template("index.html", embeddings = embeddings)
         return render_template("index.html")
     # elif user submits the form on the page
     elif request.method == "POST":
